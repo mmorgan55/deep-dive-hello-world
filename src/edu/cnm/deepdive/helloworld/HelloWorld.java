3882442655284d18e05ad936bc227ab4cfdf3816
Matthew Morgan <mmorgan55@cnm.edu>
2018-08-28 13:06:25 -0600

    New project files, with file implementing HelloWorld program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * Simple class that will print out the phrase "Hello World!"
+ * to the console.
+ *
+ */
+public class HelloWorld {
+
+  /**
+   * This method will print the phrase "Hello World" to the
+   * console.
+   * 
+   * @param args Command-line parameters (Not Used)
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello World!");
+  }
+
+}
